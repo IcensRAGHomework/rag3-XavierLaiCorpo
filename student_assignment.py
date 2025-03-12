9a7cbdb32296ad42a479c2844ad4fb913e21812d
xavier_lai <xavier_lai@asus.com>
2025-03-12 15:05:56 +0800

    HW3-1 - v1

--- student_assignment.py
+++ student_assignment.py
@@ -1,6 +1,7 @@
 import datetime
 import chromadb
 import traceback
+import pandas
 
 from chromadb.utils import embedding_functions
 
@@ -10,9 +11,46 @@ gpt_emb_version = 'text-embedding-ada-002'
 gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
+csv_path = "./COA_OpenData.csv"
 
 def generate_hw01():
-    pass
+    chroma_client = chromadb.PersistentClient(path=dbpath)
+    openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
+    )
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL",
+        metadata={"hnsw:space": "cosine"},
+        embedding_function=openai_ef
+    )
+    
+    if collection.count == 0:
+        db = pandas.read_csv(csv_path)
+
+        for idx, row in db.iterrows():
+            metadata = {
+                "file_name": csv_path,
+                "name": row["Name"],
+                "type": row["Type"],
+                "address": row["Address"],
+                "tel": row["Tel"],
+                "city": row["City"],
+                "town": row["Town"],
+                "date": int(datetime.datetime.strptime(row['CreateDate'], '%Y-%m-%d').timestamp())
+            }
+            print(f"{idx} {metadata["name"]}")
+            collection.add(
+                ids=[str(idx)],
+                metadatas=[metadata],
+                documents=[row["HostWords"]]
+            )
+        
+    return collection
+
     
 def generate_hw02(question, city, store_type, start_date, end_date):
     pass
@@ -36,3 +74,5 @@ def demo(question):
     )
     
     return collection
+
+#generate_hw01()
